af365d351c2b64350d8fea3b4d1fda30c70e3157
Jason Bariamis <jasonbari646@gmail.com>
2023-10-26 23:55:55 +0300

    comment out practice file and start first stack creation

---
+++ data-structures-stacks-queues/exercise1-stack-implementation-linked-lists.js
@@ -0,0 +1,30 @@
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(){
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek() {
+  }
+  push(value){
+  }
+  pop(){
+  }
+  //isEmpty
+}
+
+const myStack = new Stack();
+
+
+
+
+//Discord
+//Udemy
+//google
