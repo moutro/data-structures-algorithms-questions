d619e27ce1b951777643180b0371077620586785
Jason Bariamis <jasonbari646@gmail.com>
2023-10-27 23:47:45 +0300

    completed exercise1 Stack implementation for linked lists

--- data-structures-stacks-queues/exercise1-stack-implementation-linked-lists.js
+++ data-structures-stacks-queues/exercise1-stack-implementation-linked-lists.js
@@ -12,18 +12,47 @@ class Stack {
     this.length = 0;
   }
   peek() {
+    return this.top;
   }
   push(value){
+    const newNode = new Node(value); {
+        if (this.length === 0) {
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            const holdingPointer = this.top;
+            this.top = newNode;
+            this.top.next = holdingPointer;
+        }
+    }
+    this.length++;
+    return this;
   }
+
   pop(){
+    if (!this.top) {
+        return null;
+    }
+    if (this.top === this.bottom) {
+        this.bottom = null;
+    }
+    const holdingPointer = this.top;
+    this.top = this.top.next;
+    this.length--;
+    return this;
   }
   //isEmpty
 }
 
 const myStack = new Stack();
 
-
-
+console.log(myStack.push('google'));
+console.log(myStack.push('udemy'));
+console.log(myStack.push('Discord'));
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.pop());
 
 //Discord
 //Udemy
